3152dc6b83faff30e77d12398d8c199dc60dc9c2
poojakp94 <poojakprasad94@gmail.com>
2020-08-30 17:47:48 +0530

    Updates

---
+++ precache-manifest.127d858f9f107ff526b1315596b7b87d.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8eac1291085545a53a90f104687ddac2",
+    "url": "/myscoot-event/index.html"
+  },
+  {
+    "revision": "60e18d0354cb38261186",
+    "url": "/myscoot-event/static/css/main.7c4a3dae.chunk.css"
+  },
+  {
+    "revision": "4eae51b7f53e4be42cc1",
+    "url": "/myscoot-event/static/js/2.c3bae57e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myscoot-event/static/js/2.c3bae57e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60e18d0354cb38261186",
+    "url": "/myscoot-event/static/js/main.964f4d77.chunk.js"
+  },
+  {
+    "revision": "27351426af7e76d2d435",
+    "url": "/myscoot-event/static/js/runtime-main.a09b1754.js"
+  },
+  {
+    "revision": "57508b6030b67e21c2b9c919117ad534",
+    "url": "/myscoot-event/static/media/game1.57508b60.jpg"
+  },
+  {
+    "revision": "017519a7a94872deac5c0c41662f02a9",
+    "url": "/myscoot-event/static/media/guitarist.017519a7.jpg"
+  },
+  {
+    "revision": "125b789c5f7904fe12c1b1e58e1d1eef",
+    "url": "/myscoot-event/static/media/person1.125b789c.jpg"
+  },
+  {
+    "revision": "e0e5f7fce6ea2843e9a1acd9d34cff56",
+    "url": "/myscoot-event/static/media/person10.e0e5f7fc.jpg"
+  },
+  {
+    "revision": "f75fecca9eda3026a9790a44ee67a078",
+    "url": "/myscoot-event/static/media/person11.f75fecca.jpg"
+  },
+  {
+    "revision": "89ebc57ed26ee4d9f397309a42a9605e",
+    "url": "/myscoot-event/static/media/person2.89ebc57e.jpg"
+  },
+  {
+    "revision": "5e007af2b8cb4a6362fa6d3c22bee861",
+    "url": "/myscoot-event/static/media/person4.5e007af2.jpg"
+  },
+  {
+    "revision": "bb277390a951436ca2e860ddc9ad7468",
+    "url": "/myscoot-event/static/media/person5.bb277390.jpg"
+  },
+  {
+    "revision": "2c27edfeca895d51e227ca055eb48c5d",
+    "url": "/myscoot-event/static/media/person6.2c27edfe.jpg"
+  },
+  {
+    "revision": "08ce589929f19f3b79cfa1a55e733346",
+    "url": "/myscoot-event/static/media/person7.08ce5899.jpg"
+  },
+  {
+    "revision": "a14b215baf0ba73404fbbad719dbd4da",
+    "url": "/myscoot-event/static/media/person8.a14b215b.jpg"
+  },
+  {
+    "revision": "82bd03f41bd7743e5a7f244b229b8845",
+    "url": "/myscoot-event/static/media/person9.82bd03f4.jpg"
+  }
+]);
